04ef1510be23ada83c910b26234124f61d67ae62
mzaini30 <muhzaini30@gmail.com>
2019-07-03 23:41:07 +0800

    oke

--- application/controllers/Tes_twig.php
+++ application/controllers/Tes_twig.php
@@ -3,7 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Tes_twig extends CI_Controller {
 	public function index(){
-		// $this->load->view('welcome_message');
 		$nama = 'Zen';
 		$this->twig->display('tes_twig/hello_world', compact('nama'));
 	}
