728505d820073ed457640d93c371bfffd78f505b
mzaini30 <muhzaini30@gmail.com>
2019-11-14 05:32:06 +0800

    oke

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('twig', 'session');
+$autoload['libraries'] = array('twig');
 
 /*
 | -------------------------------------------------------------------
